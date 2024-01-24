2dd4a8b8eb2cf0d7c0a1d883321e3438e24b746f
azadanand9798 <azadanand9798@gmail.com>
2024-01-24 13:16:40 +0530

    js variables and introduction

---
+++ project/wedding/js/intro.js
@@ -0,0 +1,20 @@
+//javascript is a programing laguage which run in browser
+//very popular
+
+// => single line comment
+
+//way of output the result
+console.log("console output", 56, "azaad");//you can out multiple values
+
+alert(85, "Hello guys")//you can't out multiple values
+
+document.write("azad kumar", 75, 45)//you can out multiple values
+
+document.querySelector('h1').innerHTML = "hello guys this is our inner html output";
+
+/*
+hello guys
+this is multiline
+comment
+jdlja
+*/
---
+++ project/wedding/js/variables.js
@@ -0,0 +1,20 @@
+//variable is a container which store information(data)
+
+//how to declear a variable
+
+let name = "azad";
+
+// let name = "suman";//you can't declear a variable more than one times
+
+console.log(name);
+
+name = "suman";
+
+console.log(name);
+
+const age = 23;
+
+// const age = 43;
+// age = 43;
+
+console.log(age);
