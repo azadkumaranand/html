59d54277d8f4bf5747477616a8b1434a3bed113c
azadanand9798 <azadanand9798@gmail.com>
2024-02-07 13:27:26 +0530

    conditional statement

---
+++ project/wedding/js/conidtionalstatement/tut1.js
@@ -0,0 +1,34 @@
+//conditional statements
+
+// if(condition){
+    
+// }else{
+
+// }
+
+// condition => true/false
+
+//if else
+let girl = "no";
+
+if(girl === "yes"){
+    console.log("I will give you tedy!");
+}else{
+    console.log("I will block her!");
+}
+
+//if else if ... else
+
+girl = "soch bhi nhi";
+
+if(girl === "yes"){
+    console.log("I Love you! tedy!");
+}else if(girl === "wait"){
+    console.log("I should wait then i will decide i should give teddy or not!")
+}else if(girl === "soch"){
+    console.log("I should buy teddy!");
+}else if(girl == "no"){
+    console.log("I should not buy teddy! you should away from her!");
+}else{
+    console.log("evey condition has been flase!")
+}
