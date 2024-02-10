d17f1e40c0bc11bea39470b08209674ef68cea4a
azadanand9798 <azadanand9798@gmail.com>
2024-02-10 14:35:45 +0530

    switch statement

---
+++ project/wedding/js/conidtionalstatement/swtich.js
@@ -0,0 +1,17 @@
+//switch statement
+
+let a = 360;
+
+switch(a){
+    case 10:
+        console.log("hello guys this is my 10 case");
+        break;
+    case 15:
+        console.log("hello guys this is my 15 case");
+        break;
+    case 20:
+        console.log("hello guys this is my 20 case"); 
+        break;
+    default:
+        console.log("hello guys this is my default case");
+}
